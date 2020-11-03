cd95d4936d80fe9ff2ad1aa0dd9c6fe4eacb3249
Povel Galfvensjö <39699946+tellier-dev@users.noreply.github.com>
2020-11-03 19:57:39 +0100

    Added menu decoration and options

--- backup_vault.py
+++ backup_vault.py
@@ -8,6 +8,9 @@ from backup_run import Backup
 
 
 def documentation():
+    print("\ninit backup")
+    print("-- Creates a new backup")
+
     print("\ninput add {path}")
     print("-- Add a path to the input directory")
     print("-- Path to folder (without braces) e.g C:/Users/Pictures")
@@ -37,28 +40,37 @@ def documentation():
     print("\noutput clear")
     print("-- Clear all output directories")
 
-
 if __name__ == '__main__':
-    print("\nBackupVault - automate your backups!")
+    print("\n#####################################################################")
+    print("#                                                                   #")
+    print("#               BackupVault - automate your backups!                #")
+    print("#                                                                   #")
+    print("#####################################################################")
+
     if not (sys.platform.startswith("win") or sys.platform.startswith("cygwin")):
         print("Error: BackupVault only supports Windows OS")
 
     inputManager = InputManager()
     outputManager = OutputManager()
 
-    print("\nexit\n-- To exit")
-    print("\nhelp\n-- For command documentation")
+    backup = Backup()
 
-    # backup = Backup()
-    # backup.run()
+    print("\n#   exit\n#   -- To exit")
+    print("#\n#   help\n#   -- For command documentation")
+
     while True:
         print("\nEnter command: ", end='')
         command = input().strip()
 
         if command == "exit":
             break
-
-        if command == "help":
+        
+        if command == "init backup":
+            if not inputManager.has_paths() or not outputManager.has_paths():
+                print("Missing input and/or output paths")
+            else:
+                backup.run()
+        elif command == "help":
             documentation()
         elif command.startswith("input add"):
             inputManager.add_path(command[9:].strip())
