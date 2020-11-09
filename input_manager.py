63f533a9119fa9a24b57a80ecb8186215cf83efc
Povel Galfvensjö <39699946+tellier-dev@users.noreply.github.com>
2020-11-09 21:26:24 +0100

    Input saved as json

--- input_manager.py
+++ input_manager.py
@@ -1,80 +1,87 @@
 from pathlib import Path
+import json
+
+def inputAsJson(name, path, size=None):
+    return {'name': name, 'path': path}
+
+def dataAsJson(data):
+    if data is None:
+        data = []
+    json_data = {}
+    json_data['directories'] = data
+    return json_data
 
 
 class InputManager:
     def __init__(self):
-        self.__file_path = "input_dirs.txt"
+        self.__file_path = "input.json"
         doc = Path(self.__file_path)
         if not doc.is_file():
-            try:
-                open(self.__file_path, 'x')
-            except IOError:
-                print("Unhandled IO Error")
+            with open(self.__file_path, 'w') as out:
+                data = {}
+                data['directories'] = []
+                json.dump(data, out, indent=4)
 
-    def add_path(self, path, name):
-        doc = open(self.__file_path, 'a')
-        doc.write("Name:{} | Path:{}\n".format(name, path))
-        doc.close()
-        print("-- Name: {} | Path: {}".format(name, path))
-        
+    def addPath(self, path, name):
+        new_data = inputAsJson(name, path)
+        data = self.getPaths()
+        data.append(new_data)
+        self.writeDataToJson(data)
+        print('Path added')
 
-    def remove_path(self, path):
-        doc = open(self.__file_path, 'r')
-        paths = doc.readlines()
-        doc.close()
-
+    def removePath(self, path):
         removed = False
-        for p in paths:
-            if p[p.index('| Path:') + 7:] == path:
+        data = self.getPaths()
+        for directory in data:
+            if directory['path'] == path:
+                data.remove(directory)
                 removed = True
-                break
 
         if not removed:
             print("> Could not find path <")
             return
 
-        paths.remove(path)
-        doc = open(self.__file_path, 'w')
-        for p in paths:
-            doc.write(p + "\n")
-        doc.close()
+        self.writeDataToJson(data)
+        print('Path has been removed')
 
     def clear(self):
-        doc = open(self.__file_path, 'w')
-        print("Input dirs have been removed.")
-        doc.close()
+        self.writeDataToJson(None)
+        print('Input directories have been removed')
 
     def list(self):
-        doc = open(self.__file_path, 'r')
-        paths = doc.readlines()
-        print()
-        for path in paths:
-            print("Path: {}".format(path))
-        doc.close()
+        dirs = self.getPaths()
+        if len(dirs) <= 0:
+            print('No directories exist')
+            return 
+        for directory in dirs:
+            print("Path: {}".format(directory['path']))
 
-    def get_paths(self):
-        doc = open(self.__file_path, 'r')
-        doc_paths = doc.readlines()
-        doc.close()
-        paths = []
-        for path in doc_paths:
-            path = path.replace('\n', '')
-            index = path.index('| Path:') + 7
-            paths.append(path[index:])
-        return paths
+    def getPaths(self):
+        dirs = []
+        with open(self.__file_path, 'r') as json_file:
+            data = json.load(json_file)
+            if data['directories'] is not None and len(data['directories']) > 0:
+                dirs = data['directories']
+        return dirs
     
-    def get_name_from_root_path(self, root_path):
-        doc = open(self.__file_path, 'r')
-        doc_paths = doc.readlines()
-        doc.close()
-        for path in doc_paths:
-            path = path.replace('\n', '')
-            index = path.index('| Path:') + 7
-            if root_path == path[index:]:
-                index = path.index(' | Path:')
-                return path[len("Name:"):index]
-        return None
+    def getNameFromRootPath(self, root_path):
+        print('not implemented')
+        # doc = open(self.__file_path, 'r')
+        # doc_paths = doc.readlines()
+        # doc.close()
+        # for path in doc_paths:
+        #     path = path.replace('\n', '')
+        #     index = path.index('| Path:') + 7
+        #     if root_path == path[index:]:
+        #         index = path.index(' | Path:')
+        #         return path[len("Name:"):index]
+        # return None
     
-    def has_paths(self):
-        paths = self.get_paths()
+    def hasPaths(self):
+        paths = self.getPaths()
         return paths is not None and len(paths) > 0
+
+    def writeDataToJson(self, data):
+        with open(self.__file_path, 'w') as out:
+            json_data = dataAsJson(data)
+            json.dump(json_data, out, indent=4)
